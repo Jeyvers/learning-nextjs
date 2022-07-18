8193f7ab60d3a681e886786cb9b9cf940428c329
Jeyi Adole <erthnice@gmail.com>
2022-07-18 14:14:55 +0100

    Reverted to old build

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -30,33 +30,8 @@ const article = ({ article }) => {
 //   };
 // };
 
-export const getStaticProps = async (context) => {
-  const res = await fetch(`${server}/api/articles/${context.params.id}`);
-  const article = await res.json();
-  return {
-    props: {
-      article,
-    },
-  };
-};
-
-export const getStaticPaths = async () => {
-  const res = await fetch(`${server}/api/articles/`);
-  const articles = await res.json();
-
-  const ids = articles.map((article) => article.id);
-  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
 // export const getStaticProps = async (context) => {
-//   const res = await fetch(
-//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
-//   );
+//   const res = await fetch(`${server}/api/articles/${context.params.id}`);
 //   const article = await res.json();
 //   return {
 //     props: {
@@ -66,21 +41,46 @@ export const getStaticPaths = async () => {
 // };
 
 // export const getStaticPaths = async () => {
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+//   const res = await fetch(`${server}/api/articles/`);
 //   const articles = await res.json();
 
-//   //  this dynamically genereated all the paths for the posts, so even though we have 6 articles, if we search for the 20th, the 20th will be dynamically generated if it exists on jsonplaceholder
-//   //   therefore this method is better and fast than getServerSideProps in this context
-
-//   //  returns an array of all the ids for each article
 //   const ids = articles.map((article) => article.id);
 //   const paths = ids.map((id) => ({ params: { id: id.toString() } }));
 
 //   return {
 //     paths,
-//     // Returns 404 page if id does not exist
 //     fallback: false,
 //   };
 // };
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+  const article = await res.json();
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+  const articles = await res.json();
+
+  //  this dynamically genereated all the paths for the posts, so even though we have 6 articles, if we search for the 20th, the 20th will be dynamically generated if it exists on jsonplaceholder
+  //   therefore this method is better and fast than getServerSideProps in this context
+
+  //  returns an array of all the ids for each article
+  const ids = articles.map((article) => article.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    // Returns 404 page if id does not exist
+    fallback: false,
+  };
+};
 
 export default article;
--- pages/index.js
+++ pages/index.js
@@ -9,26 +9,8 @@ export default function Home({ articles }) {
   );
 }
 
-export const getStaticProps = async () => {
-  const res = await fetch(`${server}/api/articles/`);
-  const articles = await res.json();
-
-  return {
-    props: {
-      articles,
-    },
-  };
-};
-
-// Fetching data
-// getStaticProps fetches data in build time
-// getServerSideProps fetches data on every request
-// getStaticPaths dynamically generate paths based on teh data we're fetching
-
 // export const getStaticProps = async () => {
-//   const res = await fetch(
-//     'https://jsonplaceholder.typicode.com/posts?_limit=6'
-//   );
+//   const res = await fetch(`${server}/api/articles/`);
 //   const articles = await res.json();
 
 //   return {
@@ -37,3 +19,21 @@ export const getStaticProps = async () => {
 //     },
 //   };
 // };
+
+// Fetching data
+// getStaticProps fetches data in build time
+// getServerSideProps fetches data on every request
+// getStaticPaths dynamically generate paths based on teh data we're fetching
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/posts?_limit=6'
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
